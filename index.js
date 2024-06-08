264d03d49ccb3c4cbdd14c84ed4201fddbd2327b
Monika <130761795+monikabisht343@users.noreply.github.com>
2024-06-08 08:34:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import bodyParser from "body-parser"; //bodyparser middlewear to parse incoming request bodies, particulary useful for handling POST requests.
+import cors from "cors"; //Middleweare to enable cross-origin ResourcesSharing, which allows our application to handle request from different origins.
+import dotenv from "dotenv"; //module to load envirnment variables from a `.env` file, keeping sensitive data secure.
+import express from "express"; //A webframework for Node.js used to build application's server.
+import helmet from "helmet"; //helps secure application by setting various HTTP headers
+import morgan from "morgan"; //HTTP request logger middleweare logging requests.
+import multer from "multer"; //Multer middleweare to handle file uplods.
+import path from "path"; //provides utilites for working with fle and directory paths.
+import { fileURLToPath } from "url"; //fileURLToPath: a utility to get the file path from a `file:` URL.
+
+/* CONFIGURATIONS */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(cors());
+app.use("/assets", express.static(path.json(__dirname, 'public/assets')));
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb)
+{
+    cb(null, "public/assets");
+}, 
+filename: function (req, file, cb){
+    cb(null, file.originalname);
+}});
